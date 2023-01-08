773828c7b335e99bdcf905abe8860eb1a989971b
Valeriy <74008568+TsfsPro@users.noreply.github.com>
2023-01-08 17:35:20 +0300

    Игра угадай число.
    Простая консольная игра в которой угадывают число.

---
+++ ugadai.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+	cout << "Игра угадай число.!" << endl;
+	int numb = rand();
+	cout << "Число сгенереровано!" << endl;
+	cout << "Теперь очередь за вами! Введите число." << endl;
+	int usNumber = 0;
+	cin >> usNumber;
+	if (usNumber == numb) {
+		cout << "Поздравляю вы угадали." << endl;
+		cout << "Вы ввели: " << usNumber << ". А мы загадали " << numb << endl;
+	}
+	else {
+		cout << "Вы не угадали." << endl;
+		cout << "Вы ввели: " << usNumber << ". А мы загадали: " << numb << endl;
+	}
+
+}
+
+
